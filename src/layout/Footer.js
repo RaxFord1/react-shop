072d72868c5ad019063eb63d8c0a7125edcab796
Dima Dzundza <dzundzan@gmail.com>
2023-05-15 22:31:02 +0300

    sticky footer

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className="py-5 bg-dark">
+    <footer className="py-5 bg-dark mt-auto">
       <div className="container">
         <p className="m-0 text-center text-white">
           Copyright &copy; Rimuru shop 2023
--- src/layout/Template.js
+++ src/layout/Template.js
@@ -3,11 +3,11 @@ import Footer from "./Footer.js";
 
 function Template(props) {
   return (
-    <>
+    <div className="d-flex flex-column min-vh-100">
       <NavBar />
       {props.children}
       <Footer />
-    </>
+    </div>
   );
 }
 
